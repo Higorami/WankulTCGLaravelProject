c41d87665656ecd362bfb4ba4ad8b0ccfe4f0578
quaile fyleas <fyleas.quaile@edu.univ-fcomte.fr>
2025-01-03 16:10:25 +0100

    change user bdd cardClientController

--- wankulTCG/app/Http/Controllers/CardClientController.php
+++ wankulTCG/app/Http/Controllers/CardClientController.php
@@ -3,55 +3,55 @@
 namespace App\Http\Controllers;
 
 use App\Models\Card_client;
-use App\Models\Client;
+use App\Models\User;
 use App\Models\Card;
 use App\Models\Extension;
 use Illuminate\Http\Request;
 
 class CardClientController extends Controller
 {
-    // Récupérer les cartes d'un client
-    public function getCardClient($idClient)
+    // Récupérer les cartes d'un user
+    public function getCardUser($idUser)
     {
-        $client = Client::find($idClient);
-        if (!$client) {
-            return response()->json(['message' => 'Client not found'], 404);
+        $user = User::find($idUser);
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
         }
 
-        $cards = $client->cards;  // Relation définie dans le modèle Client
+        $cards = $user->cards;  // Relation définie dans le modèle User
         return response()->json($cards);
     }
 
-    // Récupérer une carte spécifique d'un client par leur ID
-    public function getCardByIDCard($idCard, $idClient)
+    // Récupérer une carte spécifique d'un user par leur ID
+    public function getCardByIDCard($idCard, $idUser)
     {
-        $cardClient = Card_client::where('client_id', $idClient)->where('card_id', $idCard)->first();
+        $cardClient = Card_client::where('user_id', $idUser)->where('card_id', $idCard)->first();
         if (!$cardClient) {
-            return response()->json(['message' => 'Card not found in client collection'], 404);
+            return response()->json(['message' => 'Card not found in user collection'], 404);
         }
 
         return response()->json($cardClient);
     }
 
-    // Ajouter une carte à la collection d'un client
-    public function addCardToCollection($idCard, $idClient)
+    // Ajouter une carte à la collection d'un user
+    public function addCardToCollection($idCard, $idUser)
     {
         $cardClient = new Card_client();
         $cardClient->card_id = $idCard;
-        $cardClient->client_id = $idClient;
+        $cardClient->user_id = $idUser;
         $cardClient->quantity = 1; // Par défaut, ajouter 1 carte
         $cardClient->save();
 
         return response()->json(['message' => 'Card added to collection']);
     }
 
-    // Augmenter la quantité d'une carte dans la collection d'un client (si la carte est déjà présente sinon on l'ajoute)
-    public function increaseCardQuantity($idCard, $idClient)
+    // Augmenter la quantité d'une carte dans la collection d'un user (si la carte est déjà présente sinon on l'ajoute)
+    public function increaseCardQuantity($idCard, $idUser)
     {
-        $cardClient = Card_client::where('client_id', $idClient)->where('card_id', $idCard)->first();
+        $cardClient = Card_client::where('ruser_id', $idUser)->where('card_id', $idCard)->first();
         if (!$cardClient) {
-            // Si la carte n'existe pas dans la collection du client, on l'ajoute
-            return $this->addCardToCollection($idCard, $idClient);
+            // Si la carte n'existe pas dans la collection du user, on l'ajoute
+            return $this->addCardToCollection($idCard, $idUser);
         }
 
         $cardClient->quantity++;
@@ -61,26 +61,26 @@ class CardClientController extends Controller
     }
 
     // Afficher la page d'ouverture de booster
-    public function getBoosterList($idClient)
+    public function getBoosterList($idUser)
     {
-        // récupérer info client
-        $client = Client::find($idClient);
-        if (!$client) {
-            return response()->json(['message' => 'Client not found'], 404);
+        // récupérer info user
+        $user = User::find($idUser);
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
         }
 
         // récupérer liste extensions
         $extensions = Extension::all();
 
         // retourner la vue avec les données
-        return view('booster-list', ['client' => $client, 'extensions' => $extensions]);
+        return view('booster-list', ['user' => $user, 'extensions' => $extensions]);
     }
 
     // Ouvrir un booster
-    public function openBooster($idClient, $idExtension)
+    public function openBooster($idUser, $idExtension)
     {
-        // récupérer les cartes du client appartenant à cette extension
-        $cardsClient = Card_client::where('client_id', $idClient)->whereHas('card', function ($query) use ($idExtension) {
+        // récupérer les cartes du user appartenant à cette extension
+        $cardsClient = Card_client::where('user_id', $idUser)->whereHas('card', function ($query) use ($idExtension) {
             $query->where('extension_id', $idExtension);
         })->get();
 
@@ -91,7 +91,7 @@ class CardClientController extends Controller
         // tirage de 5 cartes communes et 1 carte non commune
         $cards = $cardsCommunes->random(5)->merge($cardsNonCommunes->random(1));
 
-        // ajouter les cartes au client ou les vendre si le client les possède déjà en 3 exemplaires
+        // ajouter les cartes au user ou les vendre si le user les possède déjà en 3 exemplaires
         foreach ($cards as $card) {
             $cardClient = $cardsClient->where('card_id', $card->id)->first();
             if ($cardClient) {
@@ -100,14 +100,14 @@ class CardClientController extends Controller
                     $cardClient->save();
                 } else {
                     // vendre la carte
-                    $client = Client::find($idClient);
-                    $client->money += $card->price;
-                    $client->save();
+                    $user = User::find($idUser);
+                    $user->money += $card->price;
+                    $user->save();
                 }
             } else {
                 $cardClient = new Card_client();
                 $cardClient->card_id = $card->id;
-                $cardClient->client_id = $idClient;
+                $cardClient->user_id = $idUser;
                 $cardClient->quantity = 1;
                 $cardClient->save();
             }
@@ -117,19 +117,19 @@ class CardClientController extends Controller
     }
 
     // Afficher la page d'achat des cartes
-    public function marketBuy($idClient)
+    public function marketBuy($idUser)
     {
-        // récupérer info client
-        $client = Client::find($idClient);
-        if (!$client) {
-            return response()->json(['message' => 'Client not found'], 404);
+        // récupérer info user
+        $user = User::find($idUser);
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
         }
 
         // récupérer toutes les cartes
         $cards = Card::all();
 
-        // filtrer les cartes déjà possédées par le client en retirant celles qui sont en quantité de 3 ou plus
-        $cardsClient = Card_client::where('client_id', $idClient)->get();
+        // filtrer les cartes déjà possédées par le user en retirant celles qui sont en quantité de 3 ou plus
+        $cardsClient = Card_client::where('user_id', $idUser)->get();
         foreach ($cardsClient as $cardClient) {
             if ($cardClient->quantity >= 3) {
                 $cards = $cards->reject(function ($card) use ($cardClient) {
@@ -139,23 +139,23 @@ class CardClientController extends Controller
         }
 
         // retourner la vue avec les données
-        return view('market', ['client' => $client, 'cards' => $cards]);
+        return view('market', ['user' => $user, 'cards' => $cards]);
     }
 
     // Afficher la page d'achat des cartes filtrées par nom
-    public function marketBuyFiltered($idClient, $name)
+    public function marketBuyFiltered($idUser, $name)
     {
-        // récupérer info client
-        $client = Client::find($idClient);
-        if (!$client) {
-            return response()->json(['message' => 'Client not found'], 404);
+        // récupérer info user
+        $user = User::find($idUser);
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
         }
 
         // récupérer toutes les cartes
         $cards = Card::where('name', 'like', '%' . $name . '%')->get();
 
         // filtrer les cartes déjà possédées par
-        $cardsClient = Card_client::where('client_id', $idClient)->get();
+        $cardsClient = Card_client::where('user_id', $idUser)->get();
         foreach ($cardsClient as $cardClient) {
             if ($cardClient->quantity >= 3) {
                 $cards = $cards->reject(function ($card) use ($cardClient) {
@@ -165,16 +165,16 @@ class CardClientController extends Controller
         }
 
         // retourner la vue avec les données
-        return view('market', ['client' => $client, 'cards' => $cards]);
+        return view('market', ['user' => $user, 'cards' => $cards]);
     }
 
     // Acheter une carte sur le marché
-    public function buyCard($idClient, $idCard)
+    public function buyCard($idUser, $idCard)
     {
-        // récupérer info client
-        $client = Client::find($idClient);
-        if (!$client) {
-            return response()->json(['message' => 'Client not found'], 404);
+        // récupérer info user
+        $user = User::find($idUser);
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
         }
 
         // récupérer info carte
@@ -183,13 +183,13 @@ class CardClientController extends Controller
             return response()->json(['message' => 'Card not found'], 404);
         }
 
-        // vérifier si le client a assez d'argent
-        if ($client->money < $card->price) {
+        // vérifier si le user a assez d'argent
+        if ($user->money < $card->price) {
             return response()->json(['message' => 'Not enough money'], 400);
         }
 
-        // vérifier si le client possède déjà la carte
-        $cardClient = Card_client::where('client_id', $idClient)->where('card_id', $idCard)->first();
+        // vérifier si le user possède déjà la carte
+        $cardClient = Card_client::where('user_id', $idUser)->where('card_id', $idCard)->first();
         if ($cardClient) {
             if ($cardClient->quantity >= 3) {
                 return response()->json(['message' => 'Card already owned'], 400);
@@ -199,14 +199,14 @@ class CardClientController extends Controller
         } else {
             $cardClient = new Card_client();
             $cardClient->card_id = $idCard;
-            $cardClient->client_id = $idClient;
+            $cardClient->user_id = $idUser;
             $cardClient->quantity = 1;
             $cardClient->save();
         }
 
-        // débiter le client
-        $client->money -= $card->price;
-        $client->save();
+        // débiter le user
+        $user->money -= $card->price;
+        $user->save();
 
         return response()->json(['message' => 'Card bought']);
     }
